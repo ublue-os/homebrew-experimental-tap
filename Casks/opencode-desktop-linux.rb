9f80ea7d8ce2b6abf9d8ac8f0a0a0e5b7032f4a8
KiKaraage <kikaraage@tuta.io>
2026-01-10 21:51:51 +0700

    fix: use arm64_linux and x86_64_linux in opencode

--- Casks/opencode-desktop-linux.rb
+++ Casks/opencode-desktop-linux.rb
@@ -2,8 +2,8 @@ cask "opencode-desktop-linux" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "1.1.12"
-  sha256 arm:   "cb5a6a9888d4d5fa979eded94b9588428a3fc3bb19052ee4020c25f1ff2ae775",
-         intel: "632ac2a71875faa3832e5b06fb03a5c0b4edf7f3e4f5c8338086e89f6b8d9c40"
+  sha256 arm64_linux:  "cb5a6a9888d4d5fa979eded94b9588428a3fc3bb19052ee4020c25f1ff2ae775",
+         x86_64_linux: "632ac2a71875faa3832e5b06fb03a5c0b4edf7f3e4f5c8338086e89f6b8d9c40"
 
   url "https://github.com/anomalyco/opencode/releases/download/v#{version}/opencode-desktop-linux-#{arch}.rpm",
       verified: "github.com/anomalyco/opencode/"
