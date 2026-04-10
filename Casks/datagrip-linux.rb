fa55fb3a25a291589262c5cb7db5c7bc818dbf3e
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2026-04-10 04:06:40 +0000

    datagrip-linux 2026.1.2

--- Casks/datagrip-linux.rb
+++ Casks/datagrip-linux.rb
@@ -3,9 +3,9 @@ cask "datagrip-linux" do
        arm:   "-aarch64"
   os linux: "linux"
 
-  version "2026.1.1,261.22158.354"
-  sha256 x86_64_linux: "5d7e4687ec137a9e04a96ee93f59c156dc664a9b9844583bc4af80515d2a3ede",
-         arm64_linux:  "07fd16e2ee353d42731752eba64ab923e645c139accc58c14c266773088d0a2d"
+  version "2026.1.2,261.23567.23"
+  sha256 x86_64_linux: "0da02a83a5dc7b5464bc433afbeec2c42ef602f0754872850563b3243f5122e6",
+         arm64_linux:  "418ed95f83a238dee49b8c230440c221c0fc1f100b016f4f4401c9359d2df983"
 
   url "https://download.jetbrains.com/datagrip/datagrip-#{version.csv.first}#{arch}.tar.gz"
   name "DataGrip"
