01953cb2efbf1c546890fe4c0bdcd8182fddb3b7
jumpyvi <jumpyvi@outlook.com>
2026-05-01 20:41:47 -0400

    Add bottle config

--- Formula/distrobox2.rb
+++ Formula/distrobox2.rb
@@ -12,6 +12,13 @@ class Distrobox2 < Formula
     regex(/^v?(\d+(?:\.\d+)+(?:-rc\.\d+)?)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/ublue-os/homebrew-experimental-tap/releases/download/distrobox2-2.0.0-rc.2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "54d8b328d9669a6bc40eab8c9436d26be13cbe5284760929f082a21e75e78548"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c4763502758da481b4870735d255a500e6d612627943fd7ef80f7d0e672f66fe"
+  end
+
   depends_on "go" => :build
 
   def install
