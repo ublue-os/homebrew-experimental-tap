7502f5c80050198b68902cc87d2a92c3307a4198
James Reilly <jreilly1821@gmail.com>
2026-06-13 06:34:03 +0100

    bluefin-cli: update URLs after transfer to tuna-os org

--- Formula/bluefin-cli.rb
+++ Formula/bluefin-cli.rb
@@ -1,10 +1,10 @@
 class BluefinCli < Formula
   desc "Bluefin's CLI tool"
-  homepage "https://github.com/hanthor/bluefin-cli"
-  url "https://github.com/hanthor/bluefin-cli/archive/refs/tags/v0.6.4.tar.gz"
+  homepage "https://github.com/tuna-os/bluefin-cli"
+  url "https://github.com/tuna-os/bluefin-cli/archive/refs/tags/v0.6.4.tar.gz"
   sha256 "5705fbdcd2c284e773b840b49545783053d37ea56d09024f711a171be814b000"
   license "Apache-2.0"
-  head "https://github.com/hanthor/bluefin-cli.git", branch: "main"
+  head "https://github.com/tuna-os/bluefin-cli.git", branch: "main"
 
   livecheck do
     url :stable
