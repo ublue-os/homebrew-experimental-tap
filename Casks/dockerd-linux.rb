4690b8f1af76a4f691ab0636ef5534148914984f
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2026-07-31 04:10:41 +0000

    dockerd-linux 29.7.0

--- Casks/dockerd-linux.rb
+++ Casks/dockerd-linux.rb
@@ -1,6 +1,6 @@
 cask "dockerd-linux" do
-  version "29.6.2"
-  sha256 "d6204aea92238e2453d5445c885b9d2e5eb8f82915568ec50edf9dbe12a3ac74"
+  version "29.7.0"
+  sha256 "5812ccd5948835af1522c331ba830e07442d4f31ebb597ff2480ff0a2c74b7bb"
 
   url "https://download.docker.com/linux/static/stable/x86_64/docker-#{version}.tgz"
   name "Dockerd"
